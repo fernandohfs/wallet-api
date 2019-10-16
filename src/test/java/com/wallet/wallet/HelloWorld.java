905b46ebe1a2a8886afe1be3d143f52bd80f97d6
Fernando Soares <fernando.soares@luizalabs.com>
2019-10-16 17:23:40 -0300

    Configuração inicial para os testes

--- src/test/java/com/wallet/wallet/HelloWorld.java
+++ src/test/java/com/wallet/wallet/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.wallet.wallet;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,10 +9,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class HelloWorld {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void testHelloWorld() {
+    assertEquals(1, 1);
+  }
 
 }
